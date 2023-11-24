94fb61c515995626fa8ddb2aba35c0a00b8ef5f9
Evgeny <sidorov@bigam.ru>
2023-11-24 11:14:17 +0300

    v2.3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET = 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b' } = process.env;
 
 const handleAuthError = (res) => {
   res.status(401).send({ message: 'Необходима авторизация' });
