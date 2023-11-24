a1d0be803896fb43b82e9cbeec9887c82e31edae
Evgeny <sidorov@bigam.ru>
2023-11-24 10:08:48 +0300

    v2.3

--- controllers/users.js
+++ controllers/users.js
@@ -17,7 +17,7 @@ const NotFoundError = require('../errors/NotFound');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 const NotUniqueError = require('../errors/NotUniqueError');
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET = 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b' } = process.env;
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
